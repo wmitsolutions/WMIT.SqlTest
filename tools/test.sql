69511a926f32c60b0b13444d1b8f264abecb1df5
Kevin Preller <kevin.preller@wmit.biz>
2018-11-23 18:36:17 +0100

    Internationalized examples

--- tools/test.sql
+++ tools/test.sql
@@ -1,3 +1,7 @@
+--create database test
+--go
+
+
 drop procedure dbo.TestProc
 go
 create procedure dbo.TestProc 
@@ -11,8 +15,8 @@ drop procedure dbo.TestProc2
 go
 create procedure dbo.TestProc2
 as
-	if 1=2 throw 60000, 'das und das nicht richtig', 1
-	if 1=1 throw 60000, 'etwas anderes nicht richtig', 1
+	if 1=2 throw 60000, 'theres something wrong with x', 1
+	if 1=1 throw 60000, 'theres something wrong with y', 1
 go
 
 
@@ -21,14 +25,15 @@ drop procedure dbo.TestProc3
 go
 create procedure dbo.TestProc3
 as
-	throw 51000, 'my err', 1
+	throw 51000, 'my error', 1
 	return 0
 go
+
 
 
 drop procedure dbo.TestProc4
 go
 create procedure dbo.TestProc4
 as
-	select * from x
+	select * from nonExistantObject
 go
